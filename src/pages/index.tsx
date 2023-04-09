a9450f866a1b2c2bae86dcbd84cfb380a56a8563
William <WilliamWelsh@users.noreply.github.com>
2023-04-09 17:43:10 -0500

    Add auto scrolling to the bottom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 interface ChunkResponse {
   choices: {
@@ -12,6 +12,12 @@ const QUESTION = "Explain Batman's Origins";
 
 const Home: NextPage = () => {
   const [answer, setAnswer] = useState<string | null>(null);
+
+  const ref = useRef<HTMLParagraphElement>(null);
+
+  const scrollToBottom = () => {
+    if (ref.current) ref.current.scrollTop = ref.current.scrollHeight;
+  };
 
   const onSubmit = () => {
     const eventSource = new EventSource(
@@ -31,6 +37,8 @@ const Home: NextPage = () => {
       if (!chunk) return;
 
       setAnswer((prev) => (prev ? prev + chunk : chunk));
+
+      scrollToBottom();
     };
 
     eventSource.onerror = () => eventSource.close();
@@ -43,14 +51,21 @@ const Home: NextPage = () => {
         <meta name="description" content="ChatGPT Effect" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 text-white">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             ChatGPT Effect
           </h1>
           <div className="flex flex-col items-center gap-4">
             <p className="text-2xl font-bold">{QUESTION}</p>
-            {answer && <p className="font-light">{answer}</p>}
+            {answer && (
+              <p
+                ref={ref}
+                className="max-h-[420px] overflow-y-scroll font-light"
+              >
+                {answer}
+              </p>
+            )}
             <button
               onClick={onSubmit}
               disabled={answer !== null}
